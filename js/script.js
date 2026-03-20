ba78c60134d09b9cbe26207be695aa23f648ccac
Juan Rodriguez Garrido <jrodgar@aulaestudio.es>
2026-03-20 20:13:22 +0100

    Modo oscuro casi terminado

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+
+    $(".moon").click(() => {
+        $("header").css("background-color", "#161d25").css("color", "white");
+        $(".hero").css("background", "#161d25").css("color", "white");
+        $(".features").css("background-color", "#1E2732").css("color", "white");
+        $(".howto").css("background", "#161d25").css("color", "white");
+        $(".step").css("background", "#1E2732").css("color", "white");
+        $(".testimonios").css("background-color", "#1E2732").css("color", "white");
+        $(".card").css("background", "#161d25").css("color", "white");
+        $(".card p").css("color", "white");
+        $(".pricing").css("background", "#161d25").css("color", "white");
+        $(".price-card").css("background", "#1E2732").css("color", "white");
+        $(".fcta2").css("background-color", "#1E2732")
+        $("footer").css("background", "#161d25").css("color", "white");
+    })
+
+
+
+
+})
